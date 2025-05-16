a3fb1ecf712d4ef4f81db488a0b42763538d84fc
Marlon Bartoli <marlonbartoli@gmail.com>
2025-05-16 07:14:32 -0300

    Criação do ClienteService

--- src/main/java/com/UnaBanco/BancoUnaFuncional/data/HistoricoEntity.java
+++ src/main/java/com/UnaBanco/BancoUnaFuncional/data/HistoricoEntity.java
@@ -4,29 +4,37 @@
  */
 package com.UnaBanco.BancoUnaFuncional.data;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  *
  * @author Marlon
  */
-
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class HistoricoEntity {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_historico;
-
-    private String operacao;
-    private Float saldo;
-
+    
     @ManyToOne
-    @JoinColumn(name = "id_cliente", nullable = false)
+    @JoinColumn(name = "id_cliente")
     private ClienteEntity cliente;
     
+    private String operacao;
+    private Float saldo;
+    
+  
 }
--- src/main/java/service/ClienteService.java
+++ src/main/java/service/ClienteService.java
@@ -4,7 +4,11 @@
  */
 package service;
 
+import com.UnaBanco.BancoUnaFuncional.data.ClienteEntity;
 import com.UnaBanco.BancoUnaFuncional.data.ClienteRepository;
+import com.UnaBanco.BancoUnaFuncional.data.HistoricoEntity;
+import com.UnaBanco.BancoUnaFuncional.data.HistoricoRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +22,36 @@ import org.springframework.stereotype.Service;
 public class ClienteService {
     
     @Autowired
+    private ClienteRepository clienteRepo;
+
+    @Autowired
+    private HistoricoRepository historicoRepo;
     
-    ClienteRepository clienteRepository;
+    public ClienteEntity login(String login, String senha){
+        return clienteRepo.findByLoginAndSenha(login, senha).orElse(null);
+    }
     
+    public ClienteEntity depositar(Integer id, Float valor){
+        ClienteEntity cliente = clienteRepo.findById(id).orElse(null);
+        cliente.setValor(cliente.getValor() + valor);
+        clienteRepo.save(cliente);
+        
+       historicoRepo.save(new HistoricoEntity(null, cliente, "Depósito", cliente.getValor()));
+       
+       return cliente;
+    }
+    
+    public ClienteEntity tranferir(Integer id, Float valor){
+        ClienteEntity cliente = clienteRepo.findById(id).orElse(null);
+        cliente.setValor(cliente.getValor() - valor);
+        clienteRepo.save(cliente);
+        
+        historicoRepo.save(new HistoricoEntity(null, cliente, "Transferencia", cliente.getValor()));
+        
+        return cliente;
+    }
+    
+    public List<HistoricoEntity> getHistorico (Integer id_cliente){
+        return historicoRepo.findByClienteIdCliente(id_cliente);
+    }
 }
